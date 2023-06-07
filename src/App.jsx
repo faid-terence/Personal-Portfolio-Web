44a4ebfe041a71d78050747212348b6c5831032b
Faid Terence JABO <faidterence@outlook.com>
2023-06-07 20:37:50 +0200

    feature:NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react"
 import Aos from "aos"
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import './App.css'
+import { Hero } from "./components/UI/Hero";
+import Services from "./components/UI/Services";
 
 
 
@@ -9,8 +14,13 @@ export default function App() {
     }, [])
 
   return (
-    <h1 className="text-indigo-600 text-2xl font-[500]">
-      Hello world!
-    </h1>
+    <>
+    <Header/>
+    <main>
+      <Hero/>
+      <Services/>
+    </main>
+    <Footer/>
+    </>
   )
 }
